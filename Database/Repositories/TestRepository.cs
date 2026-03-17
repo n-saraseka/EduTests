9fc30f0546c594b03542a2803d7bef3b251a2542
n-saraseka <nicksaraseka43@gmail.com>
2026-03-17 18:06:53 +0300

    Add GetPageAsync to TestRepository

--- Database/Repositories/TestRepository.cs
+++ Database/Repositories/TestRepository.cs
@@ -6,7 +6,7 @@ namespace EduTests.Database.Repositories;
 public class TestRepository(DatabaseContext db) : BaseRepository<Test, int>(db)
 {
     /// <summary>
-    /// Search <see cref="Test"/>s that have matching text in their name or description
+    /// Search <see cref="Test"/>s that have matching text in their name, description, or tags
     /// </summary>
     /// <param name="text">Text to match</param>
     /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe</param>
@@ -23,9 +23,20 @@ public class TestRepository(DatabaseContext db) : BaseRepository<Test, int>(db)
     /// </summary>
     /// <param name="name">Name to match</param>
     /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe</param>
-    /// <returns>List of all <see cref="Test"/>s with the <see cref="Tag"/></returns>
+    /// <returns>An <see cref="IQueryable"/> of all <see cref="Test"/>s with the <see cref="Tag"/></returns>
     /// <exception cref="TaskCanceledException">If the <see cref="CancellationToken"/> is canceled</exception>
-    public Task<List<Test>> GetAllByTagAsync(string name, CancellationToken cancellationToken) =>
-        Set.Where(t => t.Tags.Any() && t.Tags.FirstOrDefault(tag => tag.Name == name) != null)
-            .ToListAsync(cancellationToken);
+    public IQueryable<Test> GetAllByTag(string name, CancellationToken cancellationToken) =>
+        Set.Where(t => t.Tags.Any() && t.Tags.FirstOrDefault(tag => tag.Name == name) != null);
+    
+    /// <summary>
+    /// Get <see cref="Test"/>s page
+    /// </summary>
+    /// <param name="number">Page number to get <see cref="Test"/>s from</param>
+    /// <param name="count">Count of <see cref="Test"/>s per page</param>
+    /// <param name="tests">An <see cref="IQueryable"/> containing <see cref="Test"/> data</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe</param>
+    /// <returns>A list containing all <see cref="Test"/>s on this page</returns>
+    /// <exception cref="TaskCanceledException">If the <see cref="CancellationToken"/> is canceled</exception>
+    public Task<List<Test>> GetPageAsync(int number, int count, IQueryable<Test> tests, CancellationToken cancellationToken) =>
+        tests.Skip((number - 1) * count).Take(count).ToListAsync(cancellationToken);
 }
