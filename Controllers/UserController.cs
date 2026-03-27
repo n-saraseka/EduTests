cc9fbb467ca9e5efe2bf5a8e17eeb3333f99b18d
n-saraseka <nicksaraseka43@gmail.com>
2026-03-27 19:45:45 +0300

    Remove direct UserEntityToDto method from UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,11 +2,13 @@ using EduTests.ApiObjects;
 using EduTests.Database.Entities;
 using EduTests.Database.Repositories.Interfaces;
 using EduTests.Models;
+using EduTests.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EduTests.Controllers;
 
-public class UserController(IUserRepository userRepository) : Controller
+public class UserController(IUserRepository userRepository,
+    IEntityToDtoService entityToDtoService) : Controller
 {
     public async Task<IActionResult> Profile(int id, ProfileViewModel model, CancellationToken cancellationToken = default)
     {
@@ -14,29 +16,9 @@ public class UserController(IUserRepository userRepository) : Controller
         if (user == null)
             return NotFound();
         
-        var apiUser = UserEntityToDto(user);
+        var apiUser = entityToDtoService.UserEntityToDto(user);
         model.User = apiUser;
 
         return View(model);
-    }
-    
-    /// <summary>
-    /// Map <see cref="User"/> entity to <see cref="ApiUser"/> DTO
-    /// </summary>
-    /// <param name="entity">The <see cref="User"/> entity</param>
-    /// <returns>The <see cref="ApiUser"/> DTO</returns>
-    private ApiUser UserEntityToDto(User entity)
-    {
-        var apiUser = new ApiUser
-        {
-            Id = entity.Id,
-            Username = entity.Username,
-            AvatarUrl = entity.AvatarUrl,
-            Description = entity.Description,
-            RegistrationDate = entity.RegistrationDate,
-            Group = entity.Group
-        };
-        
-        return apiUser;
     }
 }
