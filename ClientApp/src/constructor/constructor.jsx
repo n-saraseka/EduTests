ee195003c4140fa17a9b4736d9f17647e93c0073
n-saraseka <nicksaraseka43@gmail.com>
2026-04-11 18:30:43 +0300

    Create basic extra settings tab for test constructor

--- ClientApp/src/constructor/constructor.jsx
+++ ClientApp/src/constructor/constructor.jsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 import TestAside from "./testAside.jsx";
 import TestTab from "./testTab.jsx";
 import ResultTab from "./resultTab.jsx";
+import ExtraSettingsTab from "./extraSettingsTab.jsx";
 
 const tabs = ["test", "result", "extra-settings", "publish"];
 
@@ -14,16 +15,26 @@ function Constructor({baseTest, user}) {
             tags: [],
             questions: [],
             results: [],
+            timeLimit: null,
+            attemptLimit: null,
             defaultResult: null
         });
     const [currentTab, setCurrentTab] = useState(0); // Default to test and questions settings
+    const fieldNames = ['timeLimit', 'attemptLimit'];
+    const [lastFocusedField, setLastFocusedField] = useState(null);
     
     function TabSwitch() {
         switch(currentTab) {
+            // General settings and questions
             case 0:
                 return <TestTab test={test} setTest={setTest}/>;
+            // Result settings
             case 1:
                 return <ResultTab test={test} setTest={setTest}/>;
+            // Extra settings
+            case 2:
+                return <ExtraSettingsTab test={test} setTest={setTest} fieldNames={fieldNames}
+                                         lastFocusedField={lastFocusedField} setLastFocusedField={setLastFocusedField}/>;
             default:
                 return <TestTab test={test} setTest={setTest}/>;
         }
---
+++ ClientApp/src/constructor/extraSettingsTab.jsx
@@ -0,0 +1,81 @@
+import {useState} from "react";
+
+function ExtraSettingsTab({test, setTest, fieldNames, lastFocusedField, setLastFocusedField}) {
+    const [usingTimeLimit, setUsingTimeLimit] = useState(test.timeLimit !== null);
+    const [usingAttemptLimit, setUsingAttemptLimit] = useState(test.attemptLimit !== null);
+    
+    const timeLimitToMinutes = (time) => {
+        const [hours, minutes, seconds] = time.split(':').map(Number);
+        return hours * 60 + minutes + seconds / 60;
+    }
+    
+    const minutesToTimeLimit = (minutes) => {
+        return new Date(parseInt(minutes) * 60 * 1000).toISOString().substring(11, 19);
+    }
+    
+    const timeLimitSwitch = () => {
+        const newUsing = !usingTimeLimit;
+        if (!newUsing) {
+            setTest(prevTest => ({...prevTest, timeLimit: null}));
+        }
+        else {
+            setTest(prevTest => ({...prevTest, timeLimit: minutesToTimeLimit(1)}));
+        }
+        setUsingTimeLimit(newUsing);
+    }
+
+    const changeTimeLimit = (event) => {
+        const newLimit = minutesToTimeLimit(parseInt(event.target.value));
+        if (test.timeLimit !== newLimit) {
+            setTest(prevTest => ({...prevTest, timeLimit: newLimit}));
+        }
+    }
+
+    const attemptLimitSwitch = () => {
+        const newUsing = !usingAttemptLimit;
+        if (!newUsing) {
+            setTest(prevTest => ({...prevTest, attemptLimit: null}));
+        }
+        else {
+            setTest(prevTest => ({...prevTest, attemptLimit: 1}));
+        }
+        setUsingAttemptLimit(newUsing);
+    }
+    
+    const changeAttemptLimit = (event) => {
+        const newLimit = parseInt(event.target.value);
+        if (test.attemptLimit !== newLimit) {
+            setTest(prevTest => ({...prevTest, attemptLimit: newLimit}));
+        }
+    }
+    
+    return (<div className="constructor-tab">
+        <h1>Дополнительные настройки</h1>
+        <div className="test-card">
+            <div className="test-card-row">
+                <label htmlFor="using-time-limit">Ограничение времени:</label>
+                <input type="checkbox" id="using-time-limit" checked={usingTimeLimit} onChange={timeLimitSwitch}/>
+                {usingTimeLimit && <>
+                    <input type="number" step={1} min={1} max={300}
+                           defaultValue={test.timeLimit === null ? 1 : timeLimitToMinutes(test.timeLimit)}
+                           id="time-limit" onChange={changeTimeLimit}
+                           onFocus={() => setLastFocusedField(fieldNames[0])} autoFocus={lastFocusedField === fieldNames[0]}/>
+                    <label htmlFor="time-limit">мин.</label>
+                </>}
+            </div>
+            <div className="test-card-row">
+                <label htmlFor="using-attempt-limit">Ограничение прохождений:</label>
+                <input type="checkbox" id="using-attempt-limit" checked={usingAttemptLimit} onChange={attemptLimitSwitch}/>
+                {usingAttemptLimit && <>
+                    <input type="number" step={1} min={1} max={10} 
+                           defaultValue={test.attemptLimit === null ? 1 : test.attemptLimit} 
+                           onChange={changeAttemptLimit} 
+                           onFocus={() => setLastFocusedField(fieldNames[1])} autoFocus={lastFocusedField === fieldNames[1]}
+                           id="attempt-limit"/>
+                </>}
+            </div>
+        </div>
+    </div>)
+}
+
+export default ExtraSettingsTab;
