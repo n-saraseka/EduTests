0d10ab7e23f4a458a3a71d1141122b1d09463dc5
n-saraseka <nicksaraseka43@gmail.com>
2026-04-11 21:24:03 +0300

    Implement result deletion

--- ClientApp/src/constructor/resultTab.jsx
+++ ClientApp/src/constructor/resultTab.jsx
@@ -1,4 +1,5 @@
 import ConstructorResult from "./constructorResult.jsx";
+import DeleteButton from "../buttons/deleteButton.jsx";
 
 function ResultTab({test, setTest}){
     const updateResult = (index, updatedResult) => {
@@ -19,13 +20,21 @@ function ResultTab({test, setTest}){
         results.push(newResult);
         setTest(prevTest => ({...prevTest, results: results}));
     }
+    
+    const removeResult = (index) => {
+        const newResults = test.results.filter((r, i) => i !== index);
+        setTest(prevTest => ({...prevTest, results: newResults}));
+    }
 
     return (<div className="constructor-tab">
         <h1>Настройки результатов</h1>
         <div className="test-card">
             <ConstructorResult isDefault={true} result={test.defaultResult} onChange={(updated) => updateDefaultResult(updated)}/>
-            {test.results.map((r, i) => <ConstructorResult key={i} isDefault={false} result={r}
-                                                      onChange={(updated) => updateResult(i, updated)}/>)}
+            {test.results.map((r, i) => <div className="test-card-row">
+                <ConstructorResult key={i} isDefault={false} result={r}
+                                   onChange={(updated) => updateResult(i, updated)}/>
+                <DeleteButton entityType="result" onDelete={() => removeResult(i)}/>
+            </div>)}
             <button className="btn btn-primary" onClick={addResult}>Добавить результат</button>
         </div>
     </div>)
