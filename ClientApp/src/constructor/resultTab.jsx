433e537a524468f69a4b27037654fa9b94352377
n-saraseka <nicksaraseka43@gmail.com>
2026-04-12 14:26:46 +0300

    Add test result validation in constructor to allow distinct results

--- ClientApp/src/constructor/resultTab.jsx
+++ ClientApp/src/constructor/resultTab.jsx
@@ -3,6 +3,12 @@ import DeleteButton from "../buttons/deleteButton.jsx";
 
 function ResultTab({test, setTest}){
     const updateResult = (index, updatedResult) => {
+        const oldResult = test.results[index];
+        const newThreshold = updatedResult.percentageThreshold;
+        const existingResult = test.results.find(r => r.percentageThreshold === newThreshold);
+        if (existingResult) {
+            updatedResult.percentageThreshold = oldResult.percentageThreshold;
+        }
         setTest(prevTest => ({...prevTest, results: prevTest.results.map((r, i) =>
                 i === index? updatedResult : r)}));
     }
@@ -12,8 +18,13 @@ function ResultTab({test, setTest}){
     }
     
     const addResult = () => {
+        const allThresholds = Array.from({length: 101}, (n, i) => i);
+        const existingThresholds = test.results.map(r => r.percentageThreshold);
+        const newThresholds = allThresholds.filter(t => !existingThresholds.includes(t));
+        const minThreshold = Math.min.apply(null, newThresholds);
+        
         const newResult = {
-            percentageThreshold: 100,
+            percentageThreshold: minThreshold,
             result: ""
         }
         let results = test.results;
@@ -30,8 +41,8 @@ function ResultTab({test, setTest}){
         <h1>Настройки результатов</h1>
         <div className="test-card">
             <ConstructorResult isDefault={true} result={test.defaultResult} onChange={(updated) => updateDefaultResult(updated)}/>
-            {test.results.map((r, i) => <div className="test-card-row">
-                <ConstructorResult key={i} isDefault={false} result={r}
+            {test.results.map((r, i) => <div className="test-card-row" key={i}>
+                <ConstructorResult isDefault={false} result={r}
                                    onChange={(updated) => updateResult(i, updated)}/>
                 <DeleteButton entityType="result" onDelete={() => removeResult(i)}/>
             </div>)}
