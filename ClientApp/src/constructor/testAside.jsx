93b2bea6dbd5f0c2450ca5ee7dd8aa39198fc2d5
n-saraseka <nicksaraseka43@gmail.com>
2026-04-12 17:33:33 +0300

    Add aside tab titles

--- ClientApp/src/constructor/testAside.jsx
+++ ClientApp/src/constructor/testAside.jsx
@@ -1,10 +1,14 @@
 function TestAside({tabs, currentTab, setCurrentTab}) {
+    const tabNames = ["Тест", "Результат", "Доп. настройки", "Опубликовать тест"]
+    
     return (<aside>
         <ul id="tabs" className="border-right">
             {tabs.map((tab, index) => (
                 <li key={index}>
                     <img src={`/files/icons/${tab}.png`} alt="Иконка вкладки" onClick={() => setCurrentTab(index)}
-                         style={{opacity: currentTab === index ? 1.0 : 0.5}}/>
+                         className={currentTab === index ? "active" : ""}/>
+                    <span className={currentTab === index ? "aside-title active" : "aside-title"}
+                          onClick={() => setCurrentTab(index)}>{tabNames[index]}</span>
                 </li>
             ))}
         </ul>
