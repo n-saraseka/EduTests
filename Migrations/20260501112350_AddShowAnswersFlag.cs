f1cf73502c5c07d021552554dbb9e06c699b48b0
n-saraseka <nicksaraseka43@gmail.com>
2026-05-01 18:28:51 +0300

    Add missing migration details

---
+++ Migrations/20260501112350_AddShowAnswersFlag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduTests.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShowAnswersFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "show_correct_answers",
+                table: "tests",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "show_correct_answers",
+                table: "tests");
+        }
+    }
+}
